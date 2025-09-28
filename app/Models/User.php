1ed2c74f5d321f7cc82b73d0f270a43ad89a6463
Mohamed Saad <mosaad05019@gmail.com>
2025-09-28 21:38:02 +0300

    try-1

--- app/Models/User.php
+++ app/Models/User.php
@@ -38,7 +38,7 @@ class User extends Authenticatable
      *
      * @return array<string, string>
      */
-    protected function casts(): array
+    protected function casts()
     {
         return [
             'email_verified_at' => 'datetime',
